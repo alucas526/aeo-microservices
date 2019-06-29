950a3f4f6560dbce36f71f7eee1e02965bed660c
Alan Lucas <alucas@gmail.com>
2019-06-29 18:46:51 -0400

    Added gateway-service from Spring Initializr.

---
+++ gateway-service/src/main/java/com/alucas/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alucas.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
